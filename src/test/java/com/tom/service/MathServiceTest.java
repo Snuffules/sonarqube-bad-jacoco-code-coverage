a8c3dea7315a44baae987a2b6cb05c13c9d10068
Snuffules <53743351+Snuffules@users.noreply.github.com>
2025-02-18 11:39:59 +0200

    Update MathServiceTest.java

--- src/test/java/com/tom/service/MathServiceTest.java
+++ src/test/java/com/tom/service/MathServiceTest.java
@@ -7,10 +7,6 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class MathServiceTest {
-
-    // This variable is intentionally unused:
-    private String neverUsed = "I am never used!";
-
     private MathService mathService;
 
     @Before
@@ -18,96 +14,9 @@ public class MathServiceTest {
         mathService = new MathService();
     }
 
-    /**
-     * Overly complex test:
-     * - Prints credentials
-     * - Empty catch block
-     * - Deeply nested ifs (high cognitive complexity)
-     */
     @Test
-    public void testComplexLogic() {
-        System.out.println("Username: " + getClassUsername() + ", Password: " + getClassPassword());
-
-        int x = 42;
-        try {
-            throw new RuntimeException("For SonarQube failing demonstration.");
-        } catch (RuntimeException e) {
-            // Empty catch block - bad practice
-        }
-
-        // Deep nesting to increase complexity metrics
-        if (x > 0) {
-            if (x < 100) {
-                if (x == 42) {
-                    if (true) {
-                        if (true) {
-                            if (true) {
-                                x++;
-                            } else {
-                                x--;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        assertThat(x, equalTo(43));
+    public void multipliesNumbers_copy() {
+        int result = mathService.multiply(2, 4);
+        assertThat(result, equalTo(8));
     }
-
-    /**
-     * Duplicate of testComplexLogic() to raise code duplication issues.
-     */
-    @Test
-    public void testComplexLogicDuplicate() {
-        System.out.println("Username: " + getClassUsername() + ", Password: " + getClassPassword());
-
-        int x = 42;
-        try {
-            throw new RuntimeException("For SonarQube failing demonstration.");
-        } catch (RuntimeException e) {
-            // Empty catch block - bad practice
-        }
-
-        if (x > 0) {
-            if (x < 100) {
-                if (x == 42) {
-                    if (true) {
-                        if (true) {
-                            if (true) {
-                                x++;
-                            } else {
-                                x--;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        assertThat(x, equalTo(43));
-    }
-
-    // Large commented-out code block => code smell
-    /*
-    public void commentedOutMethod() {
-        System.out.println("This is commented-out code, which Sonar often flags.");
-    }
-    */
-
-    // Hard-coded credential references (discouraged).
-    private String getClassUsername() {
-        return "admin";
-    }
-
-    private String getClassPassword() {
-        return "supersecret123";
-    }
-
-    // Untested method => coverage impact
-    public void untestedHelperMethod() {
-        System.out.println("I'm never tested!");
-    }
-
-    // TODO: This is a TODO comment. Sonar often flags these if left behind.
 }
